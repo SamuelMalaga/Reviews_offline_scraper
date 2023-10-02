6119d7aad567944ce457003094a1aee7e2641320
SamuelMalaga <samuelmalaga99@gmail.com>
2023-10-01 21:34:56 -0300

    final commit to finish offline demonstration

--- scraper.js
+++ scraper.js
@@ -1,7 +1,9 @@
 const pup = require('puppeteer');
+const { pgp, db } = require('./test_pgconfig');
 const fs = require('fs');
 
-const url = "https://www.google.com/maps/place/Nema+Padaria+-+Visconde+de+Piraj%C3%A1/@-22.9841517,-43.2128543,15z/data=!4m6!3m5!1s0x9bd58a0cdc1487:0x4c1eb56d62eb469b!8m2!3d-22.9841517!4d-43.2128543!16s%2Fg%2F11j20tdp78?entry=ttu";
+const url = "https://www.google.com/maps/place/Nema/@-22.9841517,-43.2128543,15z/data=!4m5!3m4!1s0x0:0x4c1eb56d62eb469b!8m2!3d-22.9841517!4d-43.2128543?shorturl=1"
+//"https://www.google.com/maps/place/Nema+Padaria+-+Visconde+de+Piraj%C3%A1/@-22.9841517,-43.2128543,15z/data=!4m6!3m5!1s0x9bd58a0cdc1487:0x4c1eb56d62eb469b!8m2!3d-22.9841517!4d-43.2128543!16s%2Fg%2F11j20tdp78?entry=ttu";
 
 function parseAndFormatXhrResponses(XhrArray){
   const reviewDataArray = [];
@@ -28,12 +30,13 @@ function parseAndFormatXhrResponses(XhrArray){
           })
         });
       const JsonFormattedArray = JSON.stringify(reviewDataArray);
-      // Escreva os dados em um arquivo JSON localmente
-      fs.writeFileSync('browserParsedResponses.json', JsonFormattedArray, 'utf-8');
-      console.log(reviewDataArray.length, " Reviews coletadas")
-      console.log('Dados parseados em browserParsedResponses.json');
+
+
 
   });
+  console.log(reviewDataArray.length, " Reviews coletadas")
+  return reviewDataArray
+
 }
 async function findAndClickOnMoreReviewButton(ButtonsList){
   //Captura todos os botões da página
@@ -47,6 +50,16 @@ async function findAndClickOnMoreReviewButton(ButtonsList){
       const buttonText = await button.evaluate(element => element.textContent);
       if(buttonText.includes('Mais avaliações')){
         buttonElement = button
+        //Pega o numero de scrolls
+        const regex = /\((\d+)\)/;
+        const correspondencia = regex.exec(buttonText);
+        if (correspondencia && correspondencia.length > 1) {
+          const numero = parseInt(correspondencia[1], 10);
+          scrollN = Math.ceil(numero / 10);
+          console.log("Resultado:", scrollN);
+        } else {
+          console.log("Nenhuma correspondência encontrada.");
+        }
         console.log("Clicando no Mais avaliações",buttonText);
         await buttonElement.click();
       }
@@ -66,32 +79,40 @@ async function findMainReviewSection(page, mainDivSelector) {
     return null;
   }
 }
-async function findInnerReviewSectionAndScroll(page, innerDivElement, scrollAll = true) {
+async function findInnerReviewSectionAndScroll(page, innerDivElement, totalScrolls) {
   if (innerDivElement) {
     console.log('Fazendo scroll na div filha');
-    await new Promise(r => setTimeout(r, 2000));
+    await new Promise(r => setTimeout(r, 4000));
+
+    console.log("scrolls a serem feitas",totalScrolls)
 
     let previousHeight = 0;
     let currentHeight = await page.evaluate(element => element.scrollTop = element.scrollHeight, innerDivElement);
 
-    if (scrollAll) {
-      while (previousHeight !== currentHeight) {
-        previousHeight = currentHeight;
-        await page.evaluate(element => element.scrollTop = element.scrollHeight, innerDivElement);
-
-        await new Promise(resolve => setTimeout(resolve, 1000));
-
-        currentHeight = await page.evaluate(element => element.scrollTop = element.scrollHeight, innerDivElement);
-      }
-    } else {
-      for (let i = 0; i < 6; i++) {
-        await page.evaluate(element => element.scrollTop = element.scrollHeight, innerDivElement);
-        await new Promise(resolve => setTimeout(resolve, 1000));
-      }
+    for (let i = 0; i < totalScrolls; i++) {
+      await page.evaluate(element => element.scrollTop = element.scrollHeight, innerDivElement);
+      await new Promise(resolve => setTimeout(resolve, 6000));
     }
-  } else {
-    console.log('Div filha com tabindex igual a -1 não encontrada dentro da div pai.');
   }
+
+  //   if (scrollAll) {
+  //     while (previousHeight !== currentHeight) {
+  //       previousHeight = currentHeight;
+  //       await page.evaluate(element => element.scrollTop = element.scrollHeight, innerDivElement);
+
+  //       await new Promise(resolve => setTimeout(resolve, 6000));
+
+  //       currentHeight = await page.evaluate(element => element.scrollTop = element.scrollHeight, innerDivElement);
+  //     }
+  //   } else {
+  //     for (let i = 0; i < 6; i++) {
+  //       await page.evaluate(element => element.scrollTop = element.scrollHeight, innerDivElement);
+  //       await new Promise(resolve => setTimeout(resolve, 6000));
+  //     }
+  //   }
+  // } else {
+  //   console.log('Div filha com tabindex igual a -1 não encontrada dentro da div pai.');
+  // }
 }
 function getTimeStamp(){
   const currentDate = new Date();
@@ -101,12 +122,44 @@ function getTimeStamp(){
 
   return currentDate
 }
+async function inserReviewDataInDb(ReviewDataArray){
+  let totalInseridos = 0;
+  let totalIgnorados = 0;
+  for (const data of ReviewDataArray) {
+    const result = await db.func('insert_review_data_2', [
+      data.reviewId,
+      data.username,
+      data.timeOfReview,
+      data.reviewContent,
+      data.reviewRating,
+      data.reviewLang
+    ]);
+    //console.log(result[0]['insert_review_data_2'].status)
+    // Atualize os totais com base no status
+    if (result[0]['insert_review_data_2'].status === 'inserted') {
+      totalInseridos++;
+    } else if (result[0]['insert_review_data_2'].status === 'ignored') {
+      totalIgnorados++;
+    } else {
+      console.error('Resposta inesperada da função');
+    }
+  }
+  console.log(`Total inseridos: ${totalInseridos}`);
+  console.log(`Total ignorados: ${totalIgnorados}`);
+}
 
 (async () =>{
   const browser = await pup.launch({
     headless:false
   });
   const page = await browser.newPage();
+
+  await page.setExtraHTTPHeaders({ 'Accept-Language': 'en' });
+
+  await page.setViewport({
+    width: 642,
+    height: 947,
+  })
 
   //const scrollAll = false;
 
@@ -117,6 +170,7 @@ function getTimeStamp(){
   const xhrRequests = [];
 
   const xhrResponses = [];
+  let scrollN = 0
 
   await page.goto(url);
 
@@ -152,7 +206,34 @@ function getTimeStamp(){
 
   const buttonsList = await page.$$('button');
 
-  await findAndClickOnMoreReviewButton(buttonsList);
+  //Itera sobre os botões da página
+  if (buttonsList.length > 0) {
+    // Itere sobre a lista de botões e faça algo com cada um deles
+    for (const button of buttonsList) {
+      //console.log(buttons.length)
+      // Execute ação para cada botão, por exemplo, pegue o texto do botão
+      const buttonText = await button.evaluate(element => element.textContent);
+      if(buttonText.includes('Mais avaliações')){
+        buttonElement = button
+        //Pega o numero de scrolls
+        const regex = /\((\d+)\)/;
+        const correspondencia = regex.exec(buttonText);
+        if (correspondencia && correspondencia.length > 1) {
+          const numero = parseInt(correspondencia[1], 10);
+          scrollN = Math.ceil(numero / 10);
+          console.log("Resultado:", scrollN);
+        } else {
+          console.log("Nenhuma correspondência encontrada.");
+        }
+        //acaba de pegar o numero de scrolls
+
+        console.log("Clicando no Mais avaliações",buttonText);
+        await buttonElement.click();
+      }
+    }
+  } else {
+    console.log('Nenhum botão encontrado na página.');
+  }
 
   const mainDivSelector = 'div[aria-label="Nema Padaria - Visconde de Pirajá"][role="main"]';
   const mainDivElement = await findMainReviewSection(page, mainDivSelector);
@@ -160,19 +241,18 @@ function getTimeStamp(){
   const innerDivSelector = 'div[tabindex="-1"]';
   const innerDivElement = mainDivElement ? await mainDivElement.$(innerDivSelector) : null;
 
-  await findInnerReviewSectionAndScroll(page, innerDivElement);
+  await findInnerReviewSectionAndScroll(page, innerDivElement, scrollN);
   // Feche o navegador quando terminar
   await browser.close();
 
-  parseAndFormatXhrResponses(xhrResponses);
+  const scrapedData = parseAndFormatXhrResponses(xhrResponses);
 
   const endTimeStamp = getTimeStamp();
 
   console.log(`Scraping finalizado em: ${endTimeStamp.getHours()}:${endTimeStamp.getMinutes()}:${endTimeStamp.getSeconds()}`);
 
-  const scrapeTimeDiffInMinutes = Math.floor((endTimeStamp - startTimeStamp)/6000);
-
-  console.log(`Scraping concluído em ${scrapeTimeDiffInMinutes} minutos`);
+  console.log('Inserindo dados no banco de dados')
+  inserReviewDataInDb(scrapedData);
 
 })();
 
